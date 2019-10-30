b4aed082d08de999cc1e850ec14cd152b7786e79
John Goiri <jg.goiri@gmail.com>
2019-10-29 23:13:15 -0700

    Implemented lattice class for easy a and b vector access

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
+#include "Eigen/Dense"
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
-#include <fstream>
-#include "Eigen/Dense"
 
 namespace io
 {
@@ -13,7 +13,7 @@ std::vector<std::string> _parse_lattice_file(const std::string& file_path)
 
     std::vector<std::string> lat_values;
     std::string value;
-    while(file_stream>>value)
+    while (file_stream >> value)
     {
         lat_values.push_back(value);
     }
@@ -23,23 +23,49 @@ std::vector<std::string> _parse_lattice_file(const std::string& file_path)
 
 Eigen::Matrix2d read_lattice_from_file(const std::string& file_path)
 {
-    auto lat_values=_parse_lattice_file(file_path);
+    auto lat_values = _parse_lattice_file(file_path);
     Eigen::Matrix2d lattice;
-    lattice(0,0)=std::stod(lat_values[0]);
-    lattice(0,1)=std::stod(lat_values[1]);
-    lattice(1,0)=std::stod(lat_values[2]);
-    lattice(1,1)=std::stod(lat_values[3]);
+    lattice(0, 0) = std::stod(lat_values[0]);
+    lattice(0, 1) = std::stod(lat_values[1]);
+    lattice(1, 0) = std::stod(lat_values[2]);
+    lattice(1, 1) = std::stod(lat_values[3]);
 
     return lattice;
 }
+} // namespace io
 
-}
+namespace xtal
+{
 
-int main(int argc, char *argv[])
+typedef Eigen::Vector2d LatticePoint;
+
+class Lattice
+{
+public:
+    Lattice(const Eigen::Matrix2d& init_vectors_as_columns_matrix)
+        : vectors_as_columns_matrix(init_vectors_as_columns_matrix)
+    {
+    }
+
+    Eigen::Vector2d a() const { return this->vectors_as_columns_matrix.col(0); }
+
+    Eigen::Vector2d b() const { return this->vectors_as_columns_matrix.col(1); }
+
+private:
+    Eigen::Matrix2d vectors_as_columns_matrix;
+};
+
+
+} // namespace xtal
+
+int main(int argc, char* argv[])
 {
     std::string lat_file(argv[1]);
-    auto lattice=io::read_lattice_from_file(lat_file);
+    auto lattice_mat = io::read_lattice_from_file(lat_file);
 
-    std::cout<<lattice<<std::endl;
+    xtal::Lattice lattice(lattice_mat);
+
+    std::cout << "a:    " << lattice.a().transpose() << std::endl;
+    std::cout << "b:    " << lattice.b().transpose() << std::endl;
     return 0;
 }
