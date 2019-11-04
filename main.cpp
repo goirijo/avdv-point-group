0937f23ce86df737ec0fa518ac1433eb8dd06643
Julija <jvinckeviciute@gmail.com>
2019-11-04 14:55:45 -0800

    Read input lattice

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-#include "Eigen/Dense"
+#include "point_group.hpp"
+//#include "Eigen/Dense"
+#include <iostream>
+
 
 int main(int argc, char *argv[])
 {
-// Read in lattice
-
-
-// Make supergrid
+        // Read in lattice
+        const std::string file_path = "/home/julija/programming/avdv-point-group/input_lattices/triangular.txt";
+        Eigen::Matrix2d lattice = PointGroupSpace::read_lattice(file_path);
+        std::cout << "Input lattice:" << std::endl;
+        std::cout << lattice << std::endl;
+        
+        // Make supergrid
 
 
 // Loop over grid and find symops
