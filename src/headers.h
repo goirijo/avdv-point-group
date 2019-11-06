d7cbeb79d43d42724b95b3b32d134189fcb4dd28
seshasaibehara <seshasaibehara@gmail.com>
2019-11-06 05:36:12 -0800

    added_function_for_comparing_floats

--- src/headers.h
+++ src/headers.h
@@ -9,5 +9,6 @@ int is_sym_op(const Matrix2f O);
 std::list<Vector2f> calc_grid_points(int radius, const Matrix2f lattice);
 std::list<Matrix2f> calc_l_primes(const std::list<Vector2f> ps);
 std::list<Matrix2f> calc_point_group(const std::list<Matrix2f> lps, const Matrix2f lattice);
+bool approximatelyEqualAbsRel(double a, double b, double absEpsilon, double relEpsilon);
 
 #endif
