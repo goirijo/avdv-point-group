bedbf0c38d81475df090c2cf99bba610fef5ef7a
Jonas Kaufman <jonas.l.kaufman@gmail.com>
2019-11-08 00:13:06 -0800

    Added point group finding, no closure check but seems to work

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 
+#define PREC 1e-6 // precision for double comparisons
+
 // Read matrix from file
 Eigen::Matrix2d read_2x2_matrix(const char* filepath)
 {
@@ -17,10 +19,11 @@ Eigen::Matrix2d read_2x2_matrix(const char* filepath)
     return m;
 }
 
-// Generate candidate lattices from a given lattice and "radius"
-std::vector<Eigen::Matrix2d> generate_candidate_lattices(const Eigen::Matrix2d& lattice, const int& rad)
+// Generate candidate lattices from a given lattice
+std::vector<Eigen::Matrix2d> generate_candidate_lattices(const Eigen::Matrix2d& lattice)
 {
     std::vector<Eigen::Matrix2d> candidates;
+    int rad = 1; // I think this can always be 1
     for (int i = -rad; i <= rad; i++)
     {
         for (int j = -rad; j <= rad; j++)
@@ -44,7 +47,38 @@ bool is_valid_symop(const Eigen::Matrix2d& symop)
 {
     Eigen::Matrix2d id;
     id.setIdentity();
-    return id.isApprox(symop * symop.transpose());
+    return id.isApprox(symop * symop.transpose(), PREC);
+}
+
+// Generate the point group of a given lattice
+std::vector<Eigen::Matrix2d> generate_point_group(const Eigen::Matrix2d& lattice)
+{
+    std::vector<Eigen::Matrix2d> point_group;
+    Eigen::Matrix2d lattice_inv = lattice.inverse();
+    for (auto& lattice_prime : generate_candidate_lattices(lattice))
+    {
+        // Compute candidate symop and check validity
+        Eigen::Matrix2d symop = lattice_prime * lattice_inv;
+        if (is_valid_symop(symop))
+        {
+            // Check uniqueness against existing symops
+            bool unique = true;
+            for (auto& existing : point_group)
+            {
+                if (symop.isApprox(existing, PREC))
+                {
+                    unique = false;
+                    break;
+                }
+            }
+            if (unique)
+            {
+                // Add the symop if valid and unique
+                point_group.emplace_back(symop);
+            }
+        }
+    }
+    return point_group;
 }
 
 int main(int argc, char* argv[])
@@ -55,6 +89,12 @@ int main(int argc, char* argv[])
         return 1;
     }
     Eigen::Matrix2d lattice = read_2x2_matrix(argv[1]);
-    std::vector<Eigen::Matrix2d> candidates = generate_candidate_lattices(lattice, 1);
+    std::cout << "Lattice:" << std::endl << lattice << std::endl << std::endl;
+    std::vector<Eigen::Matrix2d> point_group = generate_point_group(lattice);
+    std::cout << "Point group has " << point_group.size() << " elements:" << std::endl;
+    for (auto& symop : point_group)
+    {
+        std::cout << symop << std::endl << std::endl;
+    }
     return 0;
 }
