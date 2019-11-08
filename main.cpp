3c43230d48f610519b4b7c72467c455adc5d3219
Muna Saber <munasaber@ucsb.edu>
2019-11-08 12:47:43 -0800

    I got the symmetry operations of the group

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@
   //calculate a list of potentials Lprimes
    std::vector<Eigen::Matrix2f> Calculate_Lprimes(Eigen::Matrix2f lattice, int radius)  //Again may need to go with Structure
    {
-	   std::vector<Eigen::Matrix2f> Lprimes;
+	  std::vector<Eigen::Matrix2f> Lprimes;
           auto PS=calculate_gridpoints(lattice, radius);
 	  Eigen::Matrix2f MakeMatrix;
           for (auto p1 : PS)
@@ -37,39 +37,44 @@
       	       {
       		   MakeMatrix<<p1(0), p2(0), 
 			     p1(1), p2(1);  //I may have these flipped. If Im supposed to have [p1][p2] then this woulr be correct
-		   Lprimes.push_back(MakeMatrix);
+		   Lprimes.push_back(MakeMatrix.inverse());
        	 
               }
           }
           return Lprimes;  
    }
-
- //    Calculate_point_group(std::vector<std::vector<double>> lattice, int radius) //Is the type symops?
- //    {  
- //          std::vector<std::vector<double>> validsymops;
- //          std::vector<std::vector<double>> PS=calc_grid_points(lattice, radius)
- //          std::vector<std::vector<double>> Lprimes=Calculate_Lprime(lattice, radius);
- //          for (auto Lp : Lprimes)
- //          {
- //       	   SymmetryOp=Lprimes*Lattice;
- //                       if (is_symop_valid(SymmetryOp))
- //       	         	validsymops.push_back();
- //          }
- //     return validsymops;
- //    }
- //           
- //
-////    bool is_SymOp_Unitary(SymOp SymMatrix); //Look up C++ general determinant calcs
- //
+   
+ //Determine whether or not the calculated Symmetry operation is valid through comparison of S^T*S=I
    bool is_symop_valid(Eigen::Matrix2f SymMatrix)
    {
-	 // if SymMatrix.transpose()*SymMatrix==Eigen::Identity(2,2)
 	  auto Matrixcheck= SymMatrix.transpose()*SymMatrix;
-          if (!Matrixcheck.isIdentity(.001))
+          if (!Matrixcheck.isIdentity(.05))
 		 return false;
 	  else 
 		 return true; 
    }
+   //This function calculates the symmetry operations that are valid for a given lattice
+   std::vector<Eigen::Matrix2f> Calculate_point_group(Eigen::Matrix2f lattice, int radius) //Is the type symops?
+   {  
+         std::vector<Eigen::Matrix2f> validsymops;
+         auto Lprimes=Calculate_Lprimes(lattice, radius);
+         Eigen::Matrix2f SymmetryOp;
+         for (auto Lp : Lprimes)
+         {
+      	              SymmetryOp=lattice*Lp;
+                      if (is_symop_valid(SymmetryOp))
+			      
+                       {
+      	         	  validsymops.push_back(SymmetryOp);
+		       }
+         }
+    return validsymops;
+   }
+          
+
+//    bool is_SymOp_Unitary(SymOp SymMatrix); //Look up C++ general determinant calcs
+
+ 
 
  //
  //  // bool is_group_closed(SymOp SymMatrix); //
@@ -114,15 +119,13 @@
 
 int main()
 {
-         	   
-    auto lattice=Get_Eigen_lattice("input_lattices/square.txt");
+    auto lattice=Get_Eigen_lattice("input_lattices/ugly.txt");
     auto gridpoints=calculate_gridpoints(lattice,5);
-    std::cout<<lattice;
+    std::cout<<"The lattice we are considering is"<<'\n'<<lattice<<'\n'<<'\n';     	   
     auto Lprimes=Calculate_Lprimes(lattice, 5);
-
-   for (int i=0; i<Lprimes.size();i++)
-       std::cout<< '\n'<< Lprimes[i] <<'\n';	
-    //  std::cout << Get_Eigen_lattice("input_lattices/ugly.txt");
-
+    auto validsymops= Calculate_point_group(lattice, 5);  
+    std::cout<<"The valid symmetry operations are:"<<'\n'; 
+    for (int i=0; i<validsymops.size();i++)
+       std::cout<<validsymops[i] <<'\n'<<'\n';	
    return 0;
 }
