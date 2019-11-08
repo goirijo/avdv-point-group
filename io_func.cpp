65aa097b024bcc38363ee912e9dac59dff0b2c8c
rcolleen <creynolds@ucsb.edu>
2019-11-07 16:20:28 -0800

    Fixed lattice reading bug

--- io_func.cpp
+++ io_func.cpp
@@ -11,7 +11,7 @@ using namespace std;
 auto read_lattice(string filename)
 {
 //read file and take each word and use it to populate L, an Eigen matrix
-     Eigen::Matrix2f  L;
+     Eigen::Matrix2d  L;
      fstream file;
      std::string word;
      file.open(filename.c_str());
@@ -20,24 +20,25 @@ auto read_lattice(string filename)
      while(file >> word) {
            int j=count%2;
            if(count==2){i++;}
-           L(i,j)=std::stof(word);
+	   L(i,j)=std::stod(word);
+	   count++;
      }
      return L;
 }
 
 
-void print_ptgroup(const std::vector<Eigen::Matrix2f> group)
+void print_ptgroup(const std::vector<Eigen::Matrix2d> group)
 {
 //Prints the number of symmops and then lists each of them
-      int numops=size(group);
-      std::cout<< "Point Group has " numops "Symmetry Operations"<<endl;
+      int numops=group.size();
+      std::cout<< "Point Group has " << numops << "Symmetry Operations"<<endl;
 
       int count=1;
-      for(Eigen::Matrix2f op:group){
-          str::cout<<"Symmetry Op " count<<endl;
-          str::cout<<op<<endl;
+      for(Eigen::Matrix2d op:group){
+          std::cout<<"Symmetry Op " <<count<<endl;
+          std::cout<<op<<endl;
           count++;
       }
-      return 0;
+      return;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,92 @@
 #include "Eigen/Dense"
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include "io_func.cpp"
+#include <vector>
+
+using namespace std;
+
+bool is_valid(const Eigen::Matrix2d S)
+{//check if SymOp is Unitary by checking if transpose==inverse and det is +/-1
+//    Eigen::MatrixXd I = Eigen::MatrixXd::Identity(3,3);
+    Eigen::Matrix2d sst=S*S.transpose();
+
+    if (not sst.isIdentity(0.1)){ return false;}
+    auto det=S.determinant();
+    if(abs(1-det)<.1){return true;}
+    else {return false;}
+}
+
+auto create_grid_pts(const Eigen::Matrix2d L)
+{
+//generate a grid of coordinates of points with radius n.
+//each coordinate is vector pair of doubles in a 2n+1 by 2n+1 eigen matrix
+        double n=5.0;
+        int  l=2*n+1;
+	std::vector< std::vector<double>> grid;
+	for(int i=0; i<l;i++){
+		for(int j=0; j<l; j++){
+			std::vector<double> temp;
+			double ii=i;
+			double jj=j;
+			temp.push_back((ii-n)*L(0,0)+ (jj-n)*L(0,1));
+			temp.push_back((ii-n)*L(1,0)+ (jj-n)*L(1,1));
+			grid.push_back(temp);
+			}
+	}
+
+	return grid;
+}
+
+auto calc_L_primes(const std::vector< std::vector<double>> grid)
+{
+//calculate the L-primes, which are two pairs of coordinates as lattice vectors
+//returns
+	std::vector<Eigen::Matrix2d> L_prime;
+//	std::vector<double> p1, p2;
+	for(const auto p1 : grid){
+	    for( const auto p2 : grid){
+			Eigen::Matrix2d temp;
+			temp(0,0)=p1[0];
+			temp(1,0)=p1[1];
+			temp(1,0)=p2[0];
+			temp(1,1)=p2[1];
+			L_prime.push_back(temp);
+	    }
+	}
+	return L_prime;
+}
+
+auto calc_point_group(const Eigen::Matrix2d L)
+{  
+//calculate all valid SymOps for the input Lattice
+//returns vector of SymOp matrices
+//
+   	std::vector<Eigen::Matrix2d> SymOps;
+	auto pts = create_grid_pts(L);
+	auto L_primes = calc_L_primes(pts);
+	for(Eigen::Matrix2d LP:L_primes){
+		Eigen::Matrix2d temp;
+		temp=LP*LP.inverse();
+		if(is_valid(temp)){
+			SymOps.push_back(temp);
+		}
+	}
+	return SymOps;
+}
 
 int main(int argc, char *argv[])
-{
+{ 
+//main function which reads in lattice from input file and calcualted point group
+//and prints it to the screen
+
+    Eigen::Matrix2d L = read_lattice(argv[1]);
+    std::cout<<"This is the Lattice:"<<endl;
+    std::cout<<L<<endl;
+    std::vector<Eigen::Matrix2d> pt_grp;
+
+    pt_grp = calc_point_group(L);
+//    print_ptgroup(pt_grp);
     return 0;
 }
