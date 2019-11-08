5423d0ee39e5849337315e74d68d1a7a111d8d68
rcolleen <creynolds@ucsb.edu>
2019-11-07 17:18:53 -0800

    Generates ~SymOps

--- main.cpp
+++ main.cpp
@@ -10,11 +10,11 @@ using namespace std;
 bool is_valid(const Eigen::Matrix2d S)
 {//check if SymOp is Unitary by checking if transpose==inverse and det is +/-1
 //    Eigen::MatrixXd I = Eigen::MatrixXd::Identity(3,3);
-    Eigen::Matrix2d sst=S*S.transpose();
+    Eigen::Matrix2d sst=S.transpose()*S;
 
-    if (not sst.isIdentity(0.1)){ return false;}
+    if (sst.isIdentity()==false){ return false;}
     auto det=S.determinant();
-    if(abs(1-det)<.1){return true;}
+    if((1-abs(det))<.00001){return true;}
     else {return false;}
 }
 
@@ -33,6 +33,7 @@ auto create_grid_pts(const Eigen::Matrix2d L)
 			temp.push_back((ii-n)*L(0,0)+ (jj-n)*L(0,1));
 			temp.push_back((ii-n)*L(1,0)+ (jj-n)*L(1,1));
 			grid.push_back(temp);
+//			if(j==0 &&i==5){std::cout<<temp[0] << "  "<<temp[1]<<endl;}
 			}
 	}
 
@@ -44,14 +45,19 @@ auto calc_L_primes(const std::vector< std::vector<double>> grid)
 //calculate the L-primes, which are two pairs of coordinates as lattice vectors
 //returns
 	std::vector<Eigen::Matrix2d> L_prime;
-//	std::vector<double> p1, p2;
+//	std::vector<double> p1, 2;
+        int ct=0;
 	for(const auto p1 : grid){
 	    for( const auto p2 : grid){
 			Eigen::Matrix2d temp;
 			temp(0,0)=p1[0];
 			temp(1,0)=p1[1];
-			temp(1,0)=p2[0];
+			temp(0,1)=p2[0];
 			temp(1,1)=p2[1];
+//			if(ct<5){
+//				std::cout<<temp<<endl;
+//				ct++;
+//			}
 			L_prime.push_back(temp);
 	    }
 	}
@@ -68,7 +74,7 @@ auto calc_point_group(const Eigen::Matrix2d L)
 	auto L_primes = calc_L_primes(pts);
 	for(Eigen::Matrix2d LP:L_primes){
 		Eigen::Matrix2d temp;
-		temp=LP*LP.inverse();
+		temp=LP*L.inverse();
 		if(is_valid(temp)){
 			SymOps.push_back(temp);
 		}
@@ -87,6 +93,7 @@ int main(int argc, char *argv[])
     std::vector<Eigen::Matrix2d> pt_grp;
 
     pt_grp = calc_point_group(L);
-//    print_ptgroup(pt_grp);
+//    std::cout<<pt_grp.size()<<endl;
+    print_ptgroup(pt_grp);
     return 0;
 }
