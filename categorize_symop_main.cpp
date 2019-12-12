2f0a886c96c7721cbe8b4e2c3a9c74ae49fca905
rcolleen <creynolds@ucsb.edu>
2019-12-12 12:15:30 -0800

    added cpp file

--- categorize_symop_main.cpp
+++ categorize_symop_main.cpp
@@ -10,10 +10,13 @@ class Symmetry_Operation
 {
 
 public:
-
-    Symmetry_Operation(Eigen::Matrix3d cart_matrix,  Eigen::Vector3d translation);
     Eigen::Matrix3d cart_matrix;
     Eigen::Vector3d translation;
+
+    Symmetry_Operation(Eigen::Matrix3d input_matrix,  Eigen::Vector3d input_translation){
+    Eigen::Matrix3d cart_matrix =input_matrix;
+    Eigen::Vector3d translation =input_translation;
+    };
 
     
 
@@ -50,18 +53,47 @@ bool compare_diff_to_prec(Eigen::Vector3d difference) {
     }
  }
  
+Eigen::Vector3d project_translation_onto_vector(Eigen::Vector3d  eigen_vector, Eigen::Vector3d translation)
+{
+    ///projects the translation vectors onto the axis of rotations or mirror plane and returns a vector of this translation
+    double projected_component=eigen_vector.dot(translation);
+    return projected_component*eigen_vector;
 
-int num_eigenvals_equal_one(const Eigen::Matrix3d cart_matrix){
+}
+
+Eigen::Vector3d project_translation_onto_mirror(Eigen::MatrixXd eigen_vectors, Eigen::Vector3d translation)
+{
+    //for each eigen vector this call the above project onto vector function and then added these vectors together
+    Eigen::Vector3d projected_translation_vector=Eigen::Vector3d::Zero();
+    for(int i=0; i<2;i++){
+        Eigen::Vector3d temp_translation_vector =project_translation_onto_vector(eigen_vectors.col(1), translation);
+        projected_translation_vector=projected_translation_vector+temp_translation_vector;
+    }
+    return projected_translation_vector;
+}
+
+std::vector<Eigen::Vector3d>  num_eigenvals_equal_one(const Eigen::Matrix3d cart_matrix){
     // some function that evaluate eigne vlaue and counts how many ones
-    Eigen::EigenSolver<Eigen::Matrix3d> solver(cart_matrix, false);
+    // Now returns a matrix of the Eigen vectors with eigen values of 1, in column vectors
+    // need to edit so that it returns only the real components of the eigenvector
+    Eigen::EigenSolver<Eigen::Matrix3d> solver(cart_matrix, true);
     Eigen::Vector3<std::complex<double>> eigenvals = solver.eigenvalues(); 
+    Eigen::MatrixXd eigen_vectors;
+    std::vector<Eigen::Vector3d> output_eigen_vectors;
     int eigen_counter =0;
     for( int i = 0; i<3; i++){
-        if (abs(1-eigenvals(i).real())<PREC){ 
-            if (abs(eigenvals(i).imag())<PREC){eigen_counter++;
-            }}
+        if (abs(1-abs(eigenvals(i).real()))<PREC){ 
+            if (abs(eigenvals(i).imag())<PREC){
+                eigen_counter++;
+                Eigen::Vector3d temp;
+                eigen_vectors=solver.eigenvectors().col(i);
+                std::cout<<eigen_vectors<<std::endl;
+                temp=eigen_vectors.real();
+                output_eigen_vectors.push_back(temp);
+            }
+        }
     }
-    return eigen_counter;
+    return output_eigen_vectors;
     /// det (matrix -lambda*identity)==0, lambda is the eigenvalues
 }
 
@@ -81,39 +113,50 @@ std::string check_op_type(const Symmetry_Operation sym_op, const Eigen::Matrix3d
         type = "Inversion";
         return type;
     }
-    if (has_translation(sym_op.translation, lattice))
+    Eigen::MatrixXd eigen_vectors = num_eigenvals_equal_one(sym_op.cart_matrix);
+    std::cout<<eigen_vectors<<std::endl;
+    std::cout<<eigen_vectors.cols()<<std::endl;
+    if (eigen_vectors.cols() == 2)
     {
-        if (abs(1-det)<PREC)
+        if (has_translation(project_translation_onto_mirror(eigen_vectors, sym_op.translation), lattice)){
+                type = "Glide";
+                return type;
+                }
+         else {
+             type = "Mirror";
+             return type;
+         }
+    }
+    else if (eigen_vectors.cols() == 1)
+    {
+        if (abs(det-1)<PREC)
         {
-            type = "Screw";
+            if (has_translation(project_translation_onto_vector(eigen_vectors, sym_op.translation), lattice)){
+                type = "Screw";
+                return type;
+             }
+             else
+              {
+                  type = "Rotation";
+                  return type;
+              }
+         }
+         else if(abs(det+1)<PREC) 
+         {
+            type = "Improper rotation";
             return type;
-        }
-        else
-        {
-            type = "Glide";
-            return type;
-        }
-    }
-    if (abs(det-1)<PREC)
-    {
-        type = "rotation";
-        return type;
-    }
-    int total_eigenvals_equal_one = num_eigenvals_equal_one(sym_op.cart_matrix);
-    if (total_eigenvals_equal_one == 2)
-    {
-        type = "Mirror";
-        return type;
-    }
-    else if (total_eigenvals_equal_one == 1)
-    {
-        type = "Improper rotation";
-        return type;
+         }
+         else 
+         {  
+             type = "Error type 1: Type not idenitified!!!";
+             return type;
+         }
     }
     else
     {
-        type = "Error: Type not idenitified!!!";
+        type = "Error type 2: Type not idenitified!!!";
         return type;
+        
     }
 }
 
